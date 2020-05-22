f16fa8e49c3597d07fe6f9c4cba84f96da744a99
John Ferguson <john.ferguson@kognition.ai>
2020-05-22 13:03:57 -0400

    Fix the bug where you forgot to config the middleware vs. error middle ware.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { middleware, errorMiddleware } = require('@envoy/envoy-integrations-sdk');
 
 const app = express();
+
+app.use(middleware());
 app.use(errorMiddleware());
 
 app.post('/hello-options', (req, res) => {
